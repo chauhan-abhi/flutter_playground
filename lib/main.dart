dc3e6595bf1cee60ea38ab2a43683b6bf549b77e
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-03-15 01:46:40 +0530

    initial layout setup for the login screen completed

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new LoginPage(),
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: new Container(),
+    );
+  }
+}
