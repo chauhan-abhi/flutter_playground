5d381d308e290dd9176fab74beebe064fbea66a9
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-03-30 15:31:17 +0530

    functions for +,-,*,/ added

--- calculatorapp/lib/home_page.dart
+++ calculatorapp/lib/home_page.dart
@@ -6,6 +6,51 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void doMul() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = (num1 ~/ num2);
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      t1.text = "0";
+      t2.text = "0";
+      sum = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -13,14 +58,14 @@ class HomePageState extends State<HomePage> {
         title: new Text("Calculator"),
       ),
       body: new Container(
-        padding: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.all(36.0),
         child: new Center(
             child: new SingleChildScrollView(
           child: new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Text(
-                "Ouptut: ",
+                "Ouptut: $sum",
                 style: new TextStyle(
                     fontSize: 20.0,
                     fontWeight: FontWeight.bold,
@@ -29,13 +74,15 @@ class HomePageState extends State<HomePage> {
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: new InputDecoration(hintText: "Enter Number 1"),
+                controller: t1,
               ),
               new TextField(
                 keyboardType: TextInputType.number,
                 decoration: new InputDecoration(hintText: "Enter Number 1"),
+                controller: t2,
               ),
               new Padding(
-                padding: const EdgeInsets.only(top: 20.0),
+                padding: const EdgeInsets.only(top: 16.0),
               ),
               new Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -43,12 +90,12 @@ class HomePageState extends State<HomePage> {
                   new MaterialButton(
                     child: new Text("+"),
                     color: Colors.greenAccent,
-                    onPressed: () {},
+                    onPressed: doAddition,
                   ),
                   new MaterialButton(
                     child: new Text("-"),
                     color: Colors.greenAccent,
-                    onPressed: () {},
+                    onPressed: doSub,
                   ),
                 ],
               ),
@@ -58,13 +105,26 @@ class HomePageState extends State<HomePage> {
                   new MaterialButton(
                     child: new Text("*"),
                     color: Colors.greenAccent,
-                    onPressed: () {},
+                    onPressed: doMul,
                   ),
                   new MaterialButton(
                     child: new Text("/"),
                     color: Colors.greenAccent,
-                    onPressed: () {},
+                    onPressed: doDiv,
                   ),
+                ],
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new MaterialButton(
+                    child: new Text("Clear"),
+                    color: Colors.greenAccent,
+                    onPressed: doClear,
+                  )
                 ],
               )
             ],
