cde3a2419c46d03bb3fe4e51cd02e061db8c8124
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-05-27 01:10:07 +0530

    bloc class completed

--- login_stateful/lib/src/blocs/bloc.dart
+++ login_stateful/lib/src/blocs/bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
+class Bloc with Validators {
 
   // defining the StreamController of type string and not 
   // leaving the event param as dyanamic to take any value.
@@ -17,6 +18,12 @@ class Bloc {
   Function(String) get changePassword => _passwordController.sink.add;
 
   // Add data to stream 
-  Stream<String> get email => _emailController.stream;
-  Stream<String> get password => _passwordController.stream;
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password => _passwordController.stream.transform(validatePassword);
+
+  // cleanup function
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
