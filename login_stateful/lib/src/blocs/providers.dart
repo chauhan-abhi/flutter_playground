018a442b64c5fbd72e574d079f4c65cbfe018370
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-05-30 21:13:56 +0530

    scoped instance impl added using Providers and contexts

--- login_stateful/lib/src/blocs/providers.dart
+++ login_stateful/lib/src/blocs/providers.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
-import 'bloc.dart';
+import 'bloc_scopedImpl.dart';
 
 class Provider extends InheritedWidget {
   final bloc =Bloc();
 
+  Provider({Key key, Widget child}) 
+    : super(key: key, child:child);
+
   @override
   bool updateShouldNotify(_) => true;
 
-  static Bloc of(BuildContext context) {
+  /// Build context is the handle or identifier 
+  /// that locates a widged in widget hierarchy
+  static  Bloc of(BuildContext context) {
+    // this line of code asks the widget to look at the context
+    // of its parent until it gets widget of type Provider.
+    // .bloc will return  the bloc instance created on line5
+    // by the provider
     return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
   }
 
