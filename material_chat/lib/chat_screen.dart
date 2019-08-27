e8595de9413387fe44b9ea8f8df8422105a29af0
Abhijeet <abhijeet.chauhan@indiamart.com>
2019-08-27 18:12:58 +0530

    chat_message tile created

--- material_chat/lib/chat_screen.dart
+++ material_chat/lib/chat_screen.dart
@@ -6,11 +6,42 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatState extends State<ChatScreen> {
-  
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmittedResponse(String text) {
+    _textController.clear();
+  }
+
+  Widget _textComposerWidget() {
+    return new IconTheme(
+      data: new IconThemeData(color: Colors.blue),
+      child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(
+            children: <Widget>[
+              new Flexible(
+                child: new TextField(
+                  decoration:
+                      new InputDecoration.collapsed(hintText: "Send a message"),
+                  controller: _textController,
+                  onSubmitted: _handleSubmittedResponse,
+                ),
+              ),
+              new Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () =>
+                      _handleSubmittedResponse(_textController.text),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return _textComposerWidget();
   }
 }
