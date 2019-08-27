2f57c3fc83db0e9ae0a5ecd12a69d62736586da7
Abhijeet <abhijeet.chauhan@indiamart.com>
2019-08-27 17:24:03 +0530

    chat app initial setup complete

---
+++ material_chat/lib/chat_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<ChatScreen> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
