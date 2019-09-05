d3f0927fd4b96bca56e0457ea3f8d4b79700de59
Abhijeet <abhijeet.chauhan@indiamart.com>
2019-09-05 13:24:25 +0530

    data fetched from coinmarket set to list view layout

--- crypto_app/lib/home_page.dart
+++ crypto_app/lib/home_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+
 
 class HomePage extends StatefulWidget {
+   final List currencies;
+  HomePage(this.currencies);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -8,22 +14,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   List currencies;
-  @override
-  void initState() {
-    super.initState();
-    //currencies = await getCurrenciesFromApi();
-
-  }
-
-  Future<List> getCurrenciesFromApi() async {
-    String url = "";
-  }
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Crpto App"),
+        elevation: Platform.isIOS ? 0.0 : 5.0,
       ),
       body: _cryptoWidget(),
     );
@@ -31,11 +29,67 @@ class _HomePageState extends State<HomePage> {
 
   Widget _cryptoWidget() {
     return new Container(
-      child: new Flexible(
-        child: new ListView.builder(
-          itemCount: 0,
-          itemBuilder: (_, int index) {} ,
+      child: Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              itemCount: widget.currencies.length,
+              itemBuilder: (_, int index) {
+                final int i = index ~/2;
+                final Map currency = widget.currencies[index];
+                final MaterialColor color = _colors[i % _colors.length];
+                if(index.isOdd) {
+                  return new Divider();
+                }
+                return _getListItemUi(currency, color);
+              } ,
+            ),
+          ),
+        ],
+      ),
+    );
+  } 
+
+  ListTile _getListItemUi(Map currency, MaterialColor color) {
+    return new ListTile(
+      leading: new CircleAvatar(
+        backgroundColor: color,
+        child: new Text(currency['name'][0], style: new TextStyle(color: Colors.white),),  // from JSON response
+      ),
+      title: new Text(
+        currency['name'],
+        style: new TextStyle(fontWeight:  FontWeight.bold),
         ),
+      subtitle: _getSubtitleText(
+        currency["price_usd"],
+        currency['percent_change_1h']
+      ),
+      // title and two texts in subtitles
+      isThreeLine: true,
+    );
+  }
+
+  Widget _getSubtitleText(String priceUSD, String percentageChange) {
+    TextSpan priceTextWidget = new TextSpan(
+      text: "\$$priceUSD\n",
+      style: new TextStyle(color:  Colors.black)
+    );
+    String percentageChangeText = "1 hour: $percentageChange%";
+    TextSpan percentageChangeTextWidget;
+    if(double.parse(percentageChange) > 0) {
+      percentageChangeTextWidget = new TextSpan(
+        text: percentageChangeText, 
+        style: new TextStyle(color: Colors.green)
+        );
+    } else {
+      percentageChangeTextWidget = new TextSpan(
+        text: percentageChangeText, 
+        style: new TextStyle(color: Colors.red)
+        );
+    }
+    return new RichText(
+      text: new TextSpan(
+        children: [priceTextWidget, percentageChangeTextWidget]
       ),
     );
   }
--- crypto_app/lib/main.dart
+++ crypto_app/lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert' show json;
+import 'dart:io' show Platform;
 
-void main() => runApp(MyApp());
+void main() async {
+  List currencies = await getCurrenciesFromApi();
+  print(currencies);
+  runApp(new MyApp(currencies));
+
+}
 
 class MyApp extends StatelessWidget {
+  final List _currencies;
+  MyApp(this._currencies);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: new ThemeData(
-        primarySwatch: Colors.pink
+        primarySwatch: Colors.pink,   
+        primaryColor: Platform.isIOS ? Colors.grey[100] : null      
       ),
-      home: new HomePage(),
+      home: new HomePage(_currencies),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+Future<List> getCurrenciesFromApi() async {
+    String url = "https://api.coinmarketcap.com/v1/ticker/?limit=50";
+    http.Response response = await http.get(url);
+    return json.decode(response.body);
+  }
