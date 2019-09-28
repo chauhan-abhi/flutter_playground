683462478281f03f8bc07d423407854c2ed37267
Abhijeet Singh Chauhan <chauhanabhi1245@gmail.com>
2019-09-28 12:01:24 +0530

    Util widgets skeleton completed

---
+++ bottom_nav/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: new HomePage(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Utils App"),
+      ),
+      body: new AppBody()
+    );
+  }
+}
+
+class AppBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
